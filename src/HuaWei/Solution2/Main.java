1e94c1c72bc8eb2205fb60b8bf5bf27ded714ada
tzy521521 <843460950@qq.com>
2017-08-31 02:19:02 +0800

    2017.8.31

--- src/HuaWei/Solution2/Main.java
+++ src/HuaWei/Solution2/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 /**
  * Created by tzy on 2017/8/30.
+ * 输入的是有效的程序段？
  */
 public class Main {
     public static void main(String[] args) {
@@ -30,7 +31,7 @@ public class Main {
             String text3=string.substring(string.indexOf("{int")+4,string.indexOf("="));
             String text4=string.substring(string.indexOf(";")+1,string.lastIndexOf("="));
             String text5=string.substring(string.lastIndexOf("=")+1,string.lastIndexOf("+"));
-            String text6=string.substring(string.indexOf("+")+1,string.lastIndexOf("return")-1);
+            String text6=string.substring(string.indexOf("+")+1,string.lastIndexOf(";return"));
             String text7=string.substring(string.indexOf("return")+6,string.lastIndexOf(";"));
             if (text3.equals(text4)&&text3.equals(text7)&&!text1.equals(text2)&&(text1.equals(text5)&&text2.equals(text6)||text2.equals(text5)&&text1.equals(text6)))
                 return 1;
