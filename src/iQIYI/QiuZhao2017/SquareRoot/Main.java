cd0a6cef3925a37eb90ded853fcddb9b725ef0e7
“tzy521521 <843460950@qq.com>
2017-10-07 21:53:53 +0800

    2017.10.7

--- src/iQIYI/QiuZhao2017/SquareRoot/Main.java
+++ src/iQIYI/QiuZhao2017/SquareRoot/Main.java
@@ -1,10 +1,10 @@
 package iQIYI.QiuZhao2017.SquareRoot;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Created by tzy on 2017/9/10.
+ * 于是原问题就是看a*b是否能是完全平方数,详细做法见代码注释。
  */
 public class Main {
     public static void main(String[] args) {
@@ -12,22 +12,22 @@ public class Main {
         while (scanner.hasNext()){
             int n=scanner.nextInt();
             int m=scanner.nextInt();
-            System.out.println(seuerNums(n,m));
+            System.out.println(solve(n,m));
         }
     }
-    public static int seuerNums(int n,int m){
-        ArrayList<ArrayList<Integer>> arrayLists=new ArrayList<>();
-        for (int i = 1; i <=n ; i++) {
-            ArrayList<Integer> arrayList=new ArrayList<>();
-            for (int j = 1; j <=(int)Math.sqrt(m) ; j++) {
-                if (i*(int)Math.pow(j,2)<=m)
-                    arrayList.add(i*(int)Math.pow(j,2));
-            }
-            arrayLists.add(arrayList);
-        }
+    public static int solve(int n,int m){
         int sum=0;
-        for (ArrayList<Integer> a:arrayLists) {
-            sum+=a.size();
+        for (int i = 1; i <=n ; i++) {
+           int s=1;
+            for (int j = 2; j <i/j ; j++) {
+                if (i%(j*j)==0){
+                    s=j*j;
+                }
+            }
+            int r=i/s;
+            for (int k = 1; k*k*r <=m ; k++) {
+                sum++;
+            }
         }
         return sum;
     }
