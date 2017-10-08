03043f16e1a5eab684b9747ca980c380c3330409
“tzy521521 <843460950@qq.com>
2017-10-08 12:29:25 +0800

    2017.10.8

--- src/iQIYI/QiuZhao2017/SquareString/Main.java
+++ src/iQIYI/QiuZhao2017/SquareString/Main.java
@@ -10,10 +10,25 @@ public class Main {
         Scanner scanner=new Scanner(System.in);
         while (scanner.hasNext()){
             String string=scanner.next();
-            for (int i = 0; i <string.length() ; i++) {
-
+            int max=0;
+            for (int i = 1; i <string.length() ; i++) {
+                String str1=string.substring(0,i);
+                String str2=string.substring(i);
+                max=Math.max(max,LCS(str1,str2));
             }
+            System.out.println(max*2);
         }
     }
-
+    private static int LCS(String string1,String string2){
+        int[][]dp=new int[string1.length()+1][string2.length()+1];
+        for (int i = 1; i <=string1.length() ; i++) {
+            for (int j = 1; j <=string2.length() ; j++) {
+                if (string1.charAt(i-1)==string2.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1]+1;
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[string1.length()][string2.length()];
+    }
 }
