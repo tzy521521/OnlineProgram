964def7b5f84c94b893627df67617b78a7862f75
“tzy521521 <843460950@qq.com>
2017-10-11 14:13:09 +0800

    2017.10.11

---
+++ src/iQIYI/Selection/Main.java
@@ -0,0 +1,13 @@
+package iQIYI.Selection;
+
+/**
+ * Created by tzy on 2017/10/11.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Integer a=127;
+        Integer b=127;
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+    }
+}
